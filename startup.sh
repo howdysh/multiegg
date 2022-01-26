7ad8a2bf10e3ccc9771396b4c2254104c304c2da
howdysh <98482789+howdysh@users.noreply.github.com>
2022-01-26 21:43:39 +0300

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,9 @@
+toilet --gay Software
+echo Select action:
+echo 1. Install 
+echo 2. Start
+read selectaction
+if [[ "$selectaction" == "1" ]]; then
+  echo "1. Install"
+if [[ "$selectaction" == "2" ]]; then
+  echo "1. Start"
