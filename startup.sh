bc5db6c10723e43af513cdec5b2d9ebe3b062d81
howdysh <98482789+howdysh@users.noreply.github.com>
2022-01-26 21:51:02 +0300

    Add files via upload

--- startup.sh
+++ startup.sh
@@ -3,7 +3,9 @@ echo Select action:
 echo 1. Install 
 echo 2. Start
 read selectaction
-if [[ "$selectaction" == "1" ]]; then
-  echo "1. Install"
-if [[ "$selectaction" == "2" ]]; then
-  echo "1. Start"
+if [[ "$some_variable" == "good input" ]]; then
+  echo "You got the right input."
+elif [[ "$some_variable" == "ok input" ]]; then
+  echo "Close enough"
+else
+  echo "Please select number!"
